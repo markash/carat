1f587a8fd07e281236c44551e6b5df0545992654
markash <mp.ashworth@gmail.com>
2013-07-08 10:14:52 +0200

    Added initial JBatch support

---
+++ src/main/java/za/co/yellowfire/carat/web/BatchController.java
@@ -0,0 +1,40 @@
+package za.co.yellowfire.carat.web;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.JobExecution;
+import javax.batch.runtime.JobInstance;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.util.List;
+
+@Named @ViewScoped @Slf4j
+public class BatchController {
+
+    public String onSubmit() {
+
+        JobOperator operator = BatchRuntime.getJobOperator();
+        for (String jn : operator.getJobNames()) {
+            System.out.println("JN: " + jn);
+            List<JobInstance> exe = operator.getJobInstances(jn, 0, 200);
+            if (exe != null) {
+                for (JobInstance ji : exe) {
+                    System.out.println("JI: " + ji.getInstanceId());
+                    for (JobExecution je : operator.getJobExecutions(ji)) {
+                        System.out.println("JE: " + je.getExecutionId());
+                        if (je.getJobName() != null) {
+                            System.out.println("SKSK: ");
+                        } else {
+                            throw new RuntimeException("saw null for je: " + je.getExecutionId()
+                                    + ", ji: " + ji.getInstanceId() + ", jn= " + jn);
+                        }
+                    }
+                }
+            }
+        }
+
+        return "success";
+    }
+}
---
+++ src/main/resources/db/migration/V3__Batch.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS JOBSTATUS;
+DROP TABLE IF EXISTS STEPSTATUS;
+DROP TABLE IF EXISTS CHECKPOINTDATA;
+DROP TABLE IF EXISTS JOBINSTANCEDATA;
+DROP TABLE IF EXISTS EXECUTIONINSTANCEDATA;
+DROP TABLE IF EXISTS STEPEXECUTIONINSTANCEDATA;
+
+CREATE TABLE JOBINSTANCEDATA(
+  jobinstanceid		serial not null PRIMARY KEY,
+  name		character varying (512),
+  apptag VARCHAR(512)
+);
+
+CREATE TABLE EXECUTIONINSTANCEDATA(
+  jobexecid		serial not null PRIMARY KEY,
+  jobinstanceid	bigint not null REFERENCES JOBINSTANCEDATA (jobinstanceid),
+  createtime	timestamp,
+  starttime		timestamp,
+  endtime		timestamp,
+  updatetime	timestamp,
+  parameters	bytea,
+  batchstatus		character varying (512),
+  exitstatus		character varying (512)
+);
+
+CREATE TABLE STEPEXECUTIONINSTANCEDATA(
+  stepexecid			serial not null PRIMARY KEY,
+  jobexecid			bigint not null REFERENCES EXECUTIONINSTANCEDATA (jobexecid),
+  batchstatus         character varying (512),
+  exitstatus			character varying (512),
+  stepname			character varying (512),
+  readcount			integer,
+  writecount			integer,
+  commitcount         integer,
+  rollbackcount		integer,
+  readskipcount		integer,
+  processskipcount	integer,
+  filtercount			integer,
+  writeskipcount		integer,
+  startTime           timestamp,
+  endTime             timestamp,
+  persistentData		bytea
+);
+
+CREATE TABLE JOBSTATUS (
+  id		bigint not null REFERENCES JOBINSTANCEDATA (jobinstanceid),
+  obj		bytea
+);
+
+CREATE TABLE STEPSTATUS(
+  id		bigint not null REFERENCES STEPEXECUTIONINSTANCEDATA (stepexecid),
+  obj		bytea
+);
+
+CREATE TABLE CHECKPOINTDATA(
+  id		character varying (512),
+  obj		bytea
+);
