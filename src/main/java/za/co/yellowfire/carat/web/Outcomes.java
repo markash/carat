7ca041c6cb4b9143e4100d1fa4f5767557111a4f
markash <mp.ashworth@gmail.com>
2013-07-08 07:32:08 +0200

    Added Atomikos as the Transaction Manager

---
+++ src/main/java/za/co/yellowfire/carat/web/Outcomes.java
@@ -0,0 +1,8 @@
+package za.co.yellowfire.carat.web;
+
+public interface Outcomes {
+
+    public String UPDATE = "update";
+    public String CANCEL = "cancel";
+    public String ERROR = "error";
+}
