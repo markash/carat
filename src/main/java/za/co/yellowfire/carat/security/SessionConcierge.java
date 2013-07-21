935e3ae26e4d4b78888df1dcfa29920149996cda
markash <mp.ashworth@gmail.com>
2013-07-21 14:03:55 +0200

    Cleaned up bootstrap files now that webjars is loading bootstrap and jquery

---
+++ src/main/java/za/co/yellowfire/carat/security/SessionConcierge.java
@@ -0,0 +1,48 @@
+package za.co.yellowfire.carat.security;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * http://blog.eisele.net/2012/03/java-ee-6-example-enhance-security-with.html
+ */
+@Slf4j
+public final class SessionConcierge {
+//    public static boolean addSession(HttpSession session) {
+//        String account = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+//        String sessionId = session.getId();
+//        if (account != null && !getApplicationMap(session).containsKey(account)) {
+//            getApplicationMap(session).put(account, sessionId);
+//            if (log.isDebugEnabled()) {
+//                log.debug("Added Session with ID {} for user {}", sessionId, account);
+//            }
+//            return true;
+//        } else {
+//            log.error("Cannot add sessionId, because current logged in account is NULL or session already assigned!");
+//            return false;
+//        }
+//    }
+//
+//    public static void removeSession(HttpSession session) {
+//        String sessionId = session.getId();
+//        String account = getKeyByValue(getApplicationMap(session), sessionId);
+//        if (account != null) {
+//            getApplicationMap(session).remove(account);
+//            if (log.isDebugEnabled()) {
+//                log.debug("Removed Session with ID {} for user {}", sessionId, account);
+//            }
+//        }
+//    }
+//
+//    private static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+//        for (Map.Entry<T, E> entry : map.entrySet()) {
+//            if (value.equals(entry.getValue())) {
+//                return entry.getKey();
+//            }
+//        }
+//        return null;
+//    }
+}
