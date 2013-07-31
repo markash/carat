05d9ff8403738dc4eb6193cf230abc99c792b439
markash <mp.ashworth@gmail.com>
2013-07-31 08:35:13 +0200

    Changed Arquillian to use Jetty 9 container

--- src/test/java/za/co/yellowfire/carat/web/IndexControllerTest.java
+++ src/test/java/za/co/yellowfire/carat/web/IndexControllerTest.java
@@ -1,25 +1,28 @@
 package za.co.yellowfire.carat.web;
 
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pl.com.it_crowd.arquillian.mock_contexts.ViewScopeRequired;
 import za.co.yellowfire.carat.db.ItemDao;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.testng.annotations.Test;
 
 import javax.inject.Inject;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-
-public class IndexControllerTest extends Arquillian {
+@RunWith(Arquillian.class)
+public class IndexControllerTest  {
 
     @Inject
     private IndexController controller;
 
-    @Deployment
+    @Deployment @OverProtocol("Servlet 3.0")
     @SuppressWarnings("unused")
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
@@ -28,11 +31,12 @@ public class IndexControllerTest extends Arquillian {
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
-    @Test
+    @ViewScopeRequired @Test
     public void should_create_greeting() {
         assertNotNull(controller);
 
         controller.setName("Mark");
+        System.out.println("controller = " + controller.getName());
         assertEquals(controller.getName(), "Mark");
     }
 }
