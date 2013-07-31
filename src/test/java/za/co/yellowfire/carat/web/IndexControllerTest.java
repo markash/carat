a3e0400dda5bdb7ee379c6dcf87a87784529c84c
markash <mp.ashworth@gmail.com>
2013-07-31 10:24:06 +0200

    Removed pl.com.it-crowd.mock-contexts-extension

--- src/test/java/za/co/yellowfire/carat/web/IndexControllerTest.java
+++ src/test/java/za/co/yellowfire/carat/web/IndexControllerTest.java
@@ -1,20 +1,19 @@
 package za.co.yellowfire.carat.web;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OverProtocol;
 import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import pl.com.it_crowd.arquillian.mock_contexts.ViewScopeRequired;
-import za.co.yellowfire.carat.db.ItemDao;
-
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import za.co.yellowfire.carat.db.ItemDao;
 
 import javax.inject.Inject;
-import static org.junit.Assert.assertNotNull;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(Arquillian.class)
 public class IndexControllerTest  {
@@ -22,8 +21,7 @@ public class IndexControllerTest  {
     @Inject
     private IndexController controller;
 
-    @Deployment @OverProtocol("Servlet 3.0")
-    @SuppressWarnings("unused")
+    @SuppressWarnings("unused") @Deployment @OverProtocol("Servlet 3.0")
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addClass(IndexController.class)
@@ -31,12 +29,11 @@ public class IndexControllerTest  {
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
-    @ViewScopeRequired @Test
+    @Test
     public void should_create_greeting() {
         assertNotNull(controller);
 
         controller.setName("Mark");
-        System.out.println("controller = " + controller.getName());
         assertEquals(controller.getName(), "Mark");
     }
 }
