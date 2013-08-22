93681990225fd642a86c4b5d22340dfd7b629cb4
markash <mp.ashworth@gmail.com>
2013-08-22 12:56:56 +0200

    Added test for styling messages for bootstrap

--- src/main/java/za/co/yellowfire/carat/web/IndexController.java
+++ src/main/java/za/co/yellowfire/carat/web/IndexController.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -129,6 +132,16 @@ public class IndexController implements Serializable {
 
     public boolean isPreviousRendered() {
         return first > 0;
+    }
+
+    public void onLearnMore(@SuppressWarnings("unused") ActionEvent event) {
+        FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage(
+                        FacesMessage.SEVERITY_INFO,
+                        "Sample Info",
+                        "This is a detail alert"
+                ));
     }
 
 //    protected BeanManager getBeanManager() {
